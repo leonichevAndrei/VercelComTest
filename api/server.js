dc91902762c5dd4baef1a403f384ba7444855786
Andrei <leonichev.design@gmail.com>
2024-06-28 00:18:30 +0300

    final fixes

--- api/server.js
+++ api/server.js
@@ -3,6 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware for requests logging:
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // Sample data to be sent to the frontend
 const data = [
   { name: 'Tom Hanks', points: 100 },
